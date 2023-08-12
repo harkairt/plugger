56ce28deae176d1cf7082b26e562f684e77ca2ef
harkairt <32367206+harkairt@users.noreply.github.com>
2023-08-12 21:33:28 +0200

    fix: remove materialAppPlug

--- lib/src/models/plug.dart
+++ lib/src/models/plug.dart
@@ -6,7 +6,6 @@ import 'package:plugger/src/plug_wiring.dart';
 abstract class Plug {
   FutureOr<void> runAppPlug(FutureOr<void> Function() appRunner) async => appRunner();
   PlugWiring appPlug() => defaultPlugWiring;
-  Widget materialAppPlug(BuildContext context, Widget child) => child;
   Widget navigatorPlug(BuildContext context, Widget child) => child;
   Widget pagePlug(BuildContext context, Widget child) => child;
 }
